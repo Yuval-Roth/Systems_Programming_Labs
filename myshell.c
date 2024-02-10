841d66f8d5735388cbe88511f73d6f0f7e61bb17
Yuval <rothyuv@post.bgu.ac.il>
2024-02-10 02:03:07 +0200

    fixed wrong variable names

--- myshell.c
+++ myshell.c
@@ -315,7 +315,7 @@ int main(int argc, char** argv) {
         // Parse the input using parseCmdLines
         parsedCmdLine = parseCmdLines(userInput);
 
-        if (strcmp(parsedCmdLine->arguments[0], "cd") == 0 && parseCmdLines->argCount >= 2) {
+        if (strcmp(parsedCmdLine->arguments[0], "cd") == 0 && parsedCmdLine->argCount >= 2) {
             // Change the current working directory
             if (chdir(parsedCmdLine->arguments[1]) == -1) {
                 perror("chdir");
@@ -324,17 +324,17 @@ int main(int argc, char** argv) {
             continue; // Skip the execution step for "cd" command
         }
 
-        if (strcmp(parsedCmdLine->arguments[0], "suspend") == 0 && parseCmdLines->argCount >= 2) {
+        if (strcmp(parsedCmdLine->arguments[0], "suspend") == 0 && parsedCmdLine->argCount >= 2) {
             sendSignal(atoi(parsedCmdLine->arguments[1]),SIGTSTP);
             continue;
         }
 
-        if (strcmp(parsedCmdLine->arguments[0], "wakeup") == 0 && parseCmdLines->argCount >= 2) {
+        if (strcmp(parsedCmdLine->arguments[0], "wakeup") == 0 && parsedCmdLine->argCount >= 2) {
             sendSignal(atoi(parsedCmdLine->arguments[1]),SIGCONT);
             continue;
         }
 
-        if (strcmp(parsedCmdLine->arguments[0], "nuke") == 0 && parseCmdLines->argCount >= 2) {
+        if (strcmp(parsedCmdLine->arguments[0], "nuke") == 0 && parsedCmdLine->argCount >= 2) {
             sendSignal(atoi(parsedCmdLine->arguments[1]),SIGINT);
             continue;
         }
